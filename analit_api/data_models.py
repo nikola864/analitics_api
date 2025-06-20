44b8e3b4e65561ef2d9c67dcf824cb0c5ba327ee
nikola864 <kolyab89217115399@mail.ru>
2025-06-20 14:01:00 +0300

    add data_models.py

---
+++ analit_api/data_models.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class FileMetadata(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(256), nullable=False)
+    filepath = db.Column(db.string(512), nullable=False)
+    upload_date = db.Column(db.DateTime, nullable=False)
+    file_size = db.Column(db.Integer, nullable=False)
+
+    analyses = db.relationship('AnalysisResult', backref='file', lazy=True)
+
+class AnalysisResult(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    file_id = db.Column(db.Integer, db.ForeignKey('file_metadata.id'), nullable=False)
+    analysis_date = db.Column(db.DateTime, nullable=False)
+    basic_stats = db.Column(db.JSON)
+   #correlation_matrix = db.Column(db.JSON)
+
+
+
+# FileMetadata класс с помощью которого будет храниться инф-ция о файле
+# AnalysisResult класс с помощью которого будет храниться инф-ция об анализе файла
